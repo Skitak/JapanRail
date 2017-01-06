c5c013f5af42671ada31cb8c090aa924ab2094f5
Skitak <bas2205@live.fr>
2017-01-06 11:42:00 +0100

    style modified, animations added, nourriture.php added

---
+++ nourriture.php
@@ -0,0 +1,78 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+		<link rel="stylesheet" href="style/fonts.css" />
+        <link rel="stylesheet" href="style/style.css" />
+        <!--
+		<link rel="stylesheet" media="screen and (max-width: 1280px)" href="petite_resolution.css" />
+		-->
+		<title>Les paysages ou les sages du pays</title>
+    </head>
+	
+	<body>
+		<section id="page">
+		
+			<header>
+				<?php include('./include/header.html') ?>
+			</header>
+			
+			<article>
+				<h1 id="titre">Japanese Food</h1>
+				<p>Pendant votre traversée du Japon, vous risquez de vouloir vous restaurer.
+​          </br> 
+          Et oui, le train, ça creuse.
+​          </br>
+          Perdu, vous ne saurez plus comment vous orienter.
+          </br>
+          Mais nous sommes là.
+          </br>
+          Rassurez-vous, et dirigez vous vers notre menu de plats japonais qui sont comestible pour vous : touriste d’un autre monde.</p>
+				
+				<section class="basic">
+					<figure>
+						<img src="./assets/nourriture/maki.jpg"/>
+						<figcaption>Photo prise sur le site : Wikipedia</figcaption>
+					</figure>
+					<p>Maki : Le maki est un rouleau d’algue nori séché entourant du riz japonais vinaigré avec du poisson cru ou des plantes.</p>
+				</section>
+        
+        <section class="basic">
+					<figure>
+						<img src="./assets/nourriture/onigiri.jpg"/>
+						<figcaption>Photo prise sur le site : CyberStep</figcaption>
+					</figure>
+					<p>Onigiri : Les onigiris sont des boulles de riz japonais vinaigré qui se mangent généralement lors de déplacements.</p>
+				</section>
+        
+        <section class="basic">
+					<figure>
+						<img src="./assets/nourriture/yakitori.jpg"/>
+						<figcaption>Photo prise sur le site : Foodora</figcaption>
+					</figure>
+					<p>Yakitori : les yakitoris sont des brochettes de viande (généralement du poulet ) mariné dans de la sauce tare (ou sauce yakitori).</p>
+				</section>
+        
+        <section class="basic">
+					<figure>
+						<img src="./assets/nourriture/sushi.jpg"/>
+						<figcaption>Photo prise sur le site : WallPaperdsc</figcaption>
+					</figure>
+					<p>Sushi : Riz japonais couvert d’une tranche de poisson cru , généralement par du saumon. Mais il en existe aussi avec du thon rouge, du cabillaud et en de nombreuses variétés.</p>
+				</section>
+        
+        <section class="basic">
+					<figure>
+						<img src="./assets/nourriture/ramen.jpg"/>
+						<figcaption>Photo prise sur le site : InspirationSeek</figcaption>
+					</figure>
+					<p>Ramen : Le ramen est une soupe de nouilles faites avec un bouillon de poisson ou de viande.</p>
+				</section>
+			</article>
+			
+			<footer>
+				<?php include('./include/footer.html') ?>
+			</footer>
+			
+		</section>
+	</body>
+</html>
