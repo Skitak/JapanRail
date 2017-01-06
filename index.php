0f300120f9c9ac654742edc5a63618cda92a86c2
Skitak <bas2205@live.fr>
2017-01-06 11:20:10 +0100

    shit done

--- index.php
+++ index.php
@@ -1,32 +1,15 @@
-<!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset="utf-8" />
-			<link rel="stylesheet" href="style/fonts.css" />
-			<link rel="stylesheet" href="style/style.css" />
-			<link rel="stylesheet" href="style/header.css"/>
-			<!--
-			<link rel="stylesheet" media="screen and (max-width: 1280px)" href="petite_resolution.css" />
-			-->
-			<title>Template site</title>
-		</head>
-		
-		<body>
-		<section id="page">
-		
-			<?php include('include/header.html') ?>
-			
-			<h1 class="accueil">Bienvenue <br>sur <br> <mark>Japan Rail</mark> </h1>
-			
-			<article class="accueil">
-			Ici, vous trouverez de nombreux voyages en train au Japon.
-			<br>
-			Nous vous expliquerons tout de la nourriture, aux compagnies qui s’occuperont de vous pendant vos séjours.
-			<br>
-			En espérant que vous saurez apprécier l’ambiance apaisante du japon à l’instar de ce site.
-			</article>
-			
-		</section>
-	</body>
-</html>
-		
+	
+<?php include('include/header.html') ?>
+
+
+  <h1 class="accueil">Bienvenue <br>sur <br> <mark>Japan Rail</mark> </h1>
+        
+  <article class="accueil">
+    Ici, vous trouverez de nombreux voyages en train au Japon.
+    <br>
+    Nous vous expliquerons tout de la nourriture, aux compagnies qui s’occuperont de vous pendant vos séjours.
+    <br>
+    En espérant que vous saurez apprécier l’ambiance apaisante du japon à l’instar de ce site.
+</article>
+
+<?php include('include/footer.html') ?>	
