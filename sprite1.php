a26bee71e0e0a2e3f4e4c599f5110cf48c1474d8
CfwIA <23635611+CfwIA@users.noreply.github.com>
2017-01-09 14:03:13 +0100

    Text modif

--- sprite1.php
+++ sprite1.php
@@ -1,7 +1,7 @@
 <?php include('./include/header.html') ?>			
 			<article>
 				<h1 id="titre">Moyens de transport </h1>
-				<p>Il n'existe qu'un seul vrai transport :</p>
+				<p>Il n'existe qu'une seule vrai manière de conduire un train :</p>
 				
 				<img src="./assets/trains/mtd.jpg"/>
 				<img src="./assets/trains/ddd.jpg"/>
