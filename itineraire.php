cae63670382f58585c2a70f3d65a4e76daf07776
Skitak <bas2205@live.fr>
2017-01-09 06:24:39 +0100

    itineraire modified

--- itineraire.php
+++ itineraire.php
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset="utf-8" />
-			<link rel="stylesheet" href="style/fonts.css" />
-			<link rel="stylesheet" href="style/style.css" />
-			<link rel="stylesheet" href="style/header.css"/>
-			<!--
-			<link rel="stylesheet" media="screen and (max-width: 1280px)" href="petite_resolution.css" />
-			-->
-			<title>Template site</title>
-		</head>
-		
-		<body>
-		
-		<section id="page">
-
-
 <?php include('include/header.html') ?>
-
-
-
 				<section class="basic">
 					<figure>
 						<img src="./assets/trains/FujiBulletTrain.jpg">
@@ -43,12 +22,4 @@ Kintetsu Nara -> Kintetsu Nagoya via Kintetsu</br>
 Nagoya -> Inuyama via Meitetsu</br>
 Inuyama -> Nagoya -> Airport via Meitetsu</p>
 				</section>
-
-			<footer>
 			<?php require('include/footer.html') ?>
-			</footer>
-			
-		</section>
-	</body>
-
-</html>
